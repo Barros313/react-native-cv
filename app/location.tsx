b443d540e0b6fd4f9dd961123cbf79317a8504ba
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-05-30 11:32:29 -0300

    Added location api, only text

--- app/location.tsx
+++ app/location.tsx
@@ -1,10 +1,50 @@
-import React from "react";
-import { Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, Platform, View, StyleSheet } from "react-native";
+
+import * as Location from "expo-location";
+
 
 export default () => {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+
+            if (status !== 'granted') {
+                setErrorMsg('Permissão de acesso a localização do dispositivo negada.');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+
+    }, []);
+
+    let text = 'Aguardando...';
+
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    }
+
     return (
-        <>
-            <Text> Location </Text>
-        </>
+        <View style={styles.container}>
+            <Text style={styles.paragraph}> {text} </Text>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: 25,
+    },
+    paragraph: {
+      fontSize: 18,
+      textAlign: 'center',
+    },
+  });
